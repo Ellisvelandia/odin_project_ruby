5c46c63442c51648874afcba790fed16e2b1f560
Ellis velandia <eyis619@gmail.com>
2023-10-15 15:09:58 -0500

    symbols

---
+++ symbols.rb
@@ -0,0 +1,15 @@
+# "Strigs can be changed ,so every time a string is used , ruby has store it in memory
+#  even if an existing string with the same value already exists. Symbols on the other hand
+#  are stored in memory only once, making them faste in certain situations"
+# "one common application where symbol are preferred over string are the keys hashes. "
+
+:my_symbol
+
+#Symbon vs string
+
+
+puts "string" == "string"
+
+puts "string".object_id == "string".object_id
+
+puts :symbol.object_id == :symbol.object_id
